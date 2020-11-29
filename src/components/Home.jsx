ab002cd2885ebdcc9589fe39daf99cf2bbd33b9d
David <davemgj@gmail.com>
2020-11-28 22:26:00 -0800

    initial landing/home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,21 @@
 import React from "react";
-// import ProgressiveImage from "react-progressive-graceful-image";
+import ProgressiveImage from "react-progressive-graceful-image";
 import "../styles/Home.scss";
+import akin from "../assets/akin.jpg";
+import akinLow from "../assets/akinLow.jpg";
 
 const Home = () => {
   return (
     <>
-      <section>Hello</section>
+      <section className="home-container">
+        <div>
+          <ProgressiveImage src={akin} placeholder={akinLow}>
+            {(src) => (
+              <img className="landing" src={src} alt="Painting - Akin" />
+            )}
+          </ProgressiveImage>
+        </div>
+      </section>
     </>
   );
 };
