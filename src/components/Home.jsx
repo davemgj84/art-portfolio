46916f545bf4f2d1343b796106649e4545f4952d
David <davemgj@gmail.com>
2020-11-29 18:19:59 -0800

    making landing/home page responsive - added image modal - might change

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ProgressiveImage from "react-progressive-graceful-image";
+import ModalImage from "react-modal-image";
 import "../styles/Home.scss";
 import akin from "../assets/akin.jpg";
 import akinLow from "../assets/akinLow.jpg";
@@ -11,7 +12,15 @@ const Home = () => {
         <div className="image-container">
           <ProgressiveImage src={akin} placeholder={akinLow}>
             {(src) => (
-              <img className="landing-image" src={src} alt="Painting - Akin" />
+              <ModalImage
+                className="landing-image"
+                small={src}
+                large={src}
+                alt="Painting - Akin"
+                hideDownload={true}
+                hideZoom={true}
+              />
+              // <img className="landing-image" src={src} alt="Painting - Akin" />
             )}
           </ProgressiveImage>
           <caption className="caption-title">Akin</caption>
