07bd193fccdfc80884c6e292d293e117be67ebf0
David <davemgj@gmail.com>
2020-11-30 03:31:38 -0800

    create gallery portal page - setup two links to painting and sculpture - responsive - nav background change

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,11 +1,25 @@
 import React from "react";
+import { Link } from "react-router-dom";
 // import ProgressiveImage from "react-progressive-graceful-image";
 import "../styles/Gallery.scss";
 
 const Gallery = () => {
   return (
     <>
-      <section className="gallery"></section>
+      <section className="gallery">
+        <h1>
+          Galleries
+          <hr />
+        </h1>
+        <div>
+          <Link to={"/painting"}>
+            <button className="paint-btn">Painting</button>
+          </Link>
+          <Link to={"/sculpture"}>
+            <button className="sculpt-btn">Sculpture</button>
+          </Link>
+        </div>
+      </section>
     </>
   );
 };
