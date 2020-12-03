cb740d176608679281e30ba67a3c844f934c5f07
David <davemgj@gmail.com>
2020-12-02 17:51:16 -0800

    create simple carousel for image gallery - may need further improvement

--- src/components/Painting.jsx
+++ src/components/Painting.jsx
@@ -1,27 +1,55 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ProgressiveImage from "react-progressive-graceful-image";
 import galleryObjects from "../helpers/galleryObjects";
 import "../styles/Painting.scss";
 
 const Painting = () => {
+  const [index, setIndex] = useState(0);
   const artwork = galleryObjects;
+
+  useEffect(() => {}, [index]);
+
   return (
     <>
       <section className="painting">
         <div className="painting-container">
-          <ProgressiveImage src={artwork[0].high} placeholder={artwork[0].low}>
+          <ProgressiveImage
+            src={artwork[index].high}
+            placeholder={artwork[index].low}
+          >
             {(src) => (
-              <img className="slide" src={src} alt={artwork[0].title} />
+              <img className="slide" src={src} alt={artwork[index].title} />
             )}
           </ProgressiveImage>
           <div className="info">
-            <h1 className="title">{artwork[0].title}</h1>
-            <h3>{artwork[0].size}</h3>
-            <h3>{artwork[0].medium}</h3>
+            <h1 className="title">{artwork[index].title}</h1>
+            <h3>{artwork[index].size}</h3>
+            <h3>{artwork[index].medium}</h3>
           </div>
           <div className="arrow-btns">
-            <i class="fas fa-chevron-circle-left fa-2x"></i>
-            <i class="fas fa-chevron-circle-right fa-2x"></i>
+            {index !== 0 ? (
+              <i
+                onClick={() => setIndex(index - 1)}
+                className="fas fa-chevron-circle-left fa-2x"
+              ></i>
+            ) : (
+              <i
+                style={{ visibility: "hidden" }}
+                className="fas fa-chevron-circle-left fa-2x"
+              ></i>
+            )}
+
+            {index !== artwork.length - 1 ? (
+              <i
+                onClick={() => setIndex(index + 1)}
+                className="fas fa-chevron-circle-right fa-2x"
+              ></i>
+            ) : (
+              <i
+                style={{ visibility: "hidden" }}
+                className="fas fa-chevron-circle-right fa-2x"
+              ></i>
+            )}
           </div>
         </div>
       </section>
